a93d018b4422143aadbd7b459cdb4dc7a30839cb
Vincent Noël <vincent.noel@curie.fr>
2024-08-02 15:22:42 -0300

    Added ICD notebook

---
+++ ICD_phenomenological/run.sh
@@ -0,0 +1,4 @@
+PopMaBoSS -c ICD_phenomenologicalPMNewICBig.cfg -o ResICD_phenLong ICD_phenomenologicalPM.pbnd --hexfloat
+PopMaBoSS -c ICD_phenomenologicalPMNewICBig5CExp.cfg -o ResICD_phenLong5Cl ICD_phenomenologicalPM.pbnd --hexfloat
+PopMaBoSS -c ICD_phenomenologicalPMNewICBig10CExp.cfg -o ResICD_phenLong10Cl ICD_phenomenologicalPM.pbnd --hexfloat
+
